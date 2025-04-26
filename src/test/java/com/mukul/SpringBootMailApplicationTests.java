32b7bb064c1595b38fa3bfa823ebb702e6217e72
mukulkewat <mukulkewat1@gmail.com>
2025-04-26 14:34:09 +0530

    first commit

---
+++ src/test/java/com/mukul/SpringBootMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
